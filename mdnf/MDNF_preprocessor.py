a2899430d80d90c86cbf5210d9a6ccccd80811d9
nmehlman <nmehlman@usc.edu>
2022-08-02 13:17:14 -0600

    NEw curve

--- mdnf/MDNF_preprocessor.py
+++ mdnf/MDNF_preprocessor.py
@@ -134,7 +134,7 @@ class MDNF_Torch(PreprocessorPyTorch):
             
             # Fame-based normalization
             noise_frame_energy = torch.norm(n, 2, dim=1).squeeze() # Compute energy of each frame
-            mels_frame_energy = torch.norm(mels, 2, dim=1).squeeze() # Compute energy of each frame
+            mels_frame_energy = torch.norm(mels, 2, dim=1).squeeze()
             n = n * mels_frame_energy/noise_frame_energy # Normalize
             
             mels = mels + self.nf_level * n
